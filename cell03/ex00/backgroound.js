c998273e3d2b6e1493f43debfd5b341114ff4356
66140127-tt <66140127@kmitl.ac.th>
2026-02-06 18:49:07 +0000

    cell03

---
+++ cell03/ex00/backgroound.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("changeColorBtn");
+
+button.addEventListener("click", function () {
+    const randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+});
+
+function getRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
+}
